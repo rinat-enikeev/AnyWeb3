4c768c831db4f1a4bde957036639fad2c2359cdf
Rinat Enikeev <rinat.enikeev@gmail.com>
2022-12-03 10:56:38 +0200

    initial commit: hello world app with xcodegen

---
+++ Shared/App.swift
@@ -0,0 +1,19 @@
+//
+//  App.swift
+//  AnyWeb3
+//
+//  Created by Rinat Enikeev on 03.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct App: SwiftUI.App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationStack {
+                KeystoresView()
+            }
+        }
+    }
+}
